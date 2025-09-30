fbe190e9da048e34d5ee1ba43bb1dcb8eb55c6a9
LuisaTu2 <luisa.pegoraro@gmail.com>
2025-09-30 15:40:31 -0400

    [isa] first commit earthquakes

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import './App.css'
+import { EarthquakesContext } from './context/SearchSettingsContext';
+import { useState } from 'react';
+import { type Coordinates, type EarthQuake, type EarthquakeMarker, type CenterMarker } from './types/global.t';
+import ControlPanel from './components/ControlPanel/ControlPanel';
+import EarthquakeMap from './components/Map';
+import { LoadScript } from '@react-google-maps/api';
+import CreditBubble from './components/CreditBubble';
+import EarthquakesSummary from './components/EarthquakesSummary';
+import { MapContext } from './context/MapContext';
+import PulsingDotLoader from './components/PulsingDotLoader';
+
+
+const App = () => {
+  const mapApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+  // initialize search settings state
+  const [epicenter, setEpicenter] = useState<Coordinates | null>(null)
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [earthquakes, setEarthquakes] = useState<EarthQuake[]>([]);
+  const [searchRadius, setSearchRadius] = useState<number>(500);
+
+  // initialize map state
+  const [mapRef, setMapRef] = useState<React.RefObject<google.maps.Map | null> | null>(null)
+  const [markerInfo, setMarkerInfo] = useState<CenterMarker | EarthquakeMarker | null>(null)
+  const [centerMarker, setCenterMarker] = useState<google.maps.marker.AdvancedMarkerElement | undefined | null>()
+  const [centerMarkerInfo, setCenterMarkerInfo] = useState<google.maps.InfoWindow | undefined | null>()
+  const [earthquakesMarkers, setEarthquakesMarkers] = useState<google.maps.marker.AdvancedMarkerElement[]>([])
+  const [earthquakesInfos, setEarthquakesInfos] = useState<google.maps.InfoWindow[]>([])
+  const [circle, setCircle] = useState<google.maps.Circle | null>()
+
+  return (
+    <EarthquakesContext.Provider value={{epicenter, startDate, endDate, loading, earthquakes, searchRadius,
+            setEpicenter, setStartDate, setEndDate, setLoading, setEarthquakes, setSearchRadius}}>
+      <MapContext.Provider value={{mapRef, markerInfo, centerMarker, centerMarkerInfo, earthquakesMarkers, earthquakesInfos, circle,
+        setMapRef, setMarkerInfo, setCenterMarker, setCenterMarkerInfo, setEarthquakesMarkers, setEarthquakesInfos, setCircle
+      }} >
+        <div className='app'>
+          <h2 className="app-title"> quakes </h2>
+          <PulsingDotLoader visible={loading} />
+          <LoadScript googleMapsApiKey={mapApiKey!} libraries={['places', 'marker']}>
+                  <EarthquakeMap />
+                  <ControlPanel />
+                  <EarthquakesSummary />
+
+              <CreditBubble />
+          </LoadScript>
+        </div>
+      </MapContext.Provider>
+    </EarthquakesContext.Provider>
+
+  )
+}
+
+export default App
